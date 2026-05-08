587ead18ac4d20003e2ad92fd3518d9801916f34
surendrapathak01 <pathak.surendra01@gmail.com>
2026-05-08 03:31:49 +0000

    Deploying to gh-pages from @ surendrapathak01/surendrapathak01.github.io@3c671e54fb57738ebaae3825badb52f95e0183af 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "alshedivat/al-folio",
+    "data-repo": "surendrapathak01/surendrapathak01.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
